bc4b73c07a7090c44113d9eaffeef55dcca453c5
secret <sufay30281@gmail.com>
2019-10-18 10:40:04 +0800

    refactor

--- x/ibc/mock/bank/internal/types/errors.go
+++ x/ibc/mock/bank/internal/types/errors.go
@@ -6,9 +6,11 @@ import (
 
 // ibcmockbank errors reserve 100 ~ 199.
 const (
-	CodeInvalidAmount    sdk.CodeType = 101
-	CodeInvalidAddress   sdk.CodeType = 102
-	CodeInvalidReceiver  sdk.CodeType = 103
-	CodeErrSendPacket    sdk.CodeType = 104
-	CodeErrReceivePacket sdk.CodeType = 105
+	CodeInvalidAmount     sdk.CodeType = 101
+	CodeInvalidAddress    sdk.CodeType = 102
+	CodeInvalidReceiver   sdk.CodeType = 103
+	CodeErrSendPacket     sdk.CodeType = 104
+	CodeErrReceivePacket  sdk.CodeType = 105
+	CodeProofMissing      sdk.CodeType = 106
+	CodeInvalidPacketData sdk.CodeType = 107
 )
